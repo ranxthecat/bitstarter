c239ff9cb382b514c910eb204d217c9c15883f95
gregory ecker <gregory.ecker@gmail.com>
2013-07-22 20:00:24 -0400

    changed message to read from file "index.html"

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+
+var buf = new Buffer(256);
+buf = fs.readFileSync("index.html");
+var text = buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
